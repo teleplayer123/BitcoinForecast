64ad848e3e983ed93f9f8af1f10f4f5aaf5234ca
Cole Sashkin <40610719+teleplayer123@users.noreply.github.com>
2021-04-30 04:49:07 -0700

    Update build_model.py
    working on options for  building best model

--- build_model.py
+++ build_model.py
@@ -1,9 +1,8 @@
 import numpy as np 
-from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization, Activation
 from tensorflow.keras.models import Sequential
-from sklearn.preprocessing import scale
 
-def build_model(n_nodes, n_out, n_layers, in_shape, drop_rate=0.2,
+def build_n_layer_model(n_nodes, n_out, n_layers, in_shape, drop_rate=0.2,
                 loss="sparse_categorical_crossentropy", opt="adam", metrics=["accuracy"], batch_normalization=True):
     model = Sequential()
     for i in range(n_layers):
@@ -19,6 +18,29 @@ def build_model(n_nodes, n_out, n_layers, in_shape, drop_rate=0.2,
     model.add(Dense(n_out, activation="relu"))
     model.add(Dropout(drop_rate))
     model.add(Dense(2, activation="softmax"))
-    model.compile(optimizer=opt, loss=loss, metrics=metrics)
+    model.compile(optimizer=opt, loss=loss)
     return model
 
+def build_model(units):
+    model = Sequential()
+    model.add(LSTM(units, input_shape=(None, 1), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(LSTM(units, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(LSTM(units))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Dense(1))
+    model.add(Activation("linear"))
+    model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+    return model
+
+
+def create_model(units):
+    model = Sequential()
+    model.add(LSTM(units, activation="sigmoid", input_shape=(None, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    return model
