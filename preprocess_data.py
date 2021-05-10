adc8984b1200be4fd382c1e58c98bc81205fdfdb
Cole Sashkin <csashceh1024@gmail.com>
2021-05-09 22:17:58 -0700

    commit on main

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,11 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import datetime
 
+from utils import binary_classification
+
 FORECAST_STEP = 3
+
+scaler = MinMaxScaler()
 
 df = pd.read_csv("data/btc-usd-yf.csv")
 df.set_index("Date", inplace=True)
@@ -15,3 +19,7 @@ df.fillna(method="ffill", inplace=True)
 main_df = df.copy()
 main_df = main_df[["Close"]]
 main_df["Forecast"] = main_df["Close"].shift(-FORECAST_STEP)
+main_df["Class"] = list(map(binary_classification, main_df["Close"], main_df["Forecast"]))
+scaled_data = scaler.fit_transform(main_df[["Close", "Class"]])
+
+#TODO: train data should not include NaN values created by shift -forecast
