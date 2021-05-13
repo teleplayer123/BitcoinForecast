2edab83f964725cb80a4a370938e63d7c4582a4f
Cole Sashkin <csashceh1024@gmail.com>
2021-05-13 09:26:30 -0700

    commit on main

--- btc_forecast_kaggle.py
+++ btc_forecast_kaggle.py
@@ -14,13 +14,12 @@ df = pd.read_csv("data/btc-kaggle.csv")
 df["Date"] = pd.to_datetime(df["Timestamp"], unit="s").dt.date
 group_by_date = df.groupby("Date")
 prices = group_by_date["Close"].mean()
-main_df = pd.DataFrame(prices)
+test_data = prices[len(prices)-FORECAST_STEP:]
+main_df = pd.DataFrame(prices[:len(prices)-FORECAST_STEP])
 main_df["Forecast"] = main_df["Close"].shift(-FORECAST_STEP)
 
 main_df = main_df[["Close", "Forecast"]]
 scaled_data = scaler.fit_transform(main_df)
-data = scaled_data[:len(scaled_data)-FORECAST_STEP]
-test_data = scaled_data[len(scaled_data)-FORECAST_STEP:]
-X_train, X_test, y_train, y_test = preprocess_data(data, SEQ_LEN, 0.2)
 
-print(test_data)
+X_train, X_test, y_train, y_test = preprocess_data(scaled_data, SEQ_LEN, 0.2)
+
