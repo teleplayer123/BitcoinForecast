a3d76d74dca51dcf0e57fcb168de238f5287a22a
Cole Sashkin <csashceh1024@gmail.com>
2021-05-15 14:56:27 -0700

    commit on main

--- btc_forecast.py
+++ btc_forecast.py
@@ -9,6 +9,7 @@ import os
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 from build_model import build_model, build_dense_model, build_n_layer_model, simple_model, build_model_v2
+from utils import binary_classification, preprocess_data, visualize_results
 
 FEATURE_COLUMNS = ["Time", "Low", "High", "Open", "Close", "Volume"]
 SEQ_LEN = 60
@@ -32,52 +33,6 @@ if not os.path.exists("testing"):
 if not os.path.exists(f"testing/{DIR_NAME}"):
     os.mkdir(f"testing/{DIR_NAME}")
 
-
-def binary_classification(prev, forecast):
-    if prev >= forecast:
-        return 0
-    else:
-        return 1
-
-def seq_split(data, seq_len):
-    seq_data = []
-    seqs = []
-    i = 0
-    while i < len(data)-seq_len:
-        if len(seqs) == seq_len:
-            seq_data.append(seqs)
-        seqs.append(data[i:i+seq_len])
-        i += 1
-    seq_data = sum(seq_data, [])
-    return np.array(seq_data)
-
-def preprocess_data(data, seq_len, test_ratio):
-    seq_data = seq_split(data, seq_len)
-    test_size = int(len(seq_data) * test_ratio)
-    X_train = seq_data[:-test_size, :-1]
-    y_train = seq_data[:-test_size, -1]
-    X_test = seq_data[-test_size:, :-1]
-    y_test = seq_data[-test_size:, -1]
-    return X_train, X_test, y_train, y_test
-
-def visualize_results(results):
-    res = results.history
-    plt.figure(figsize=(12,4))
-    plt.plot(res["val_loss"])
-    plt.plot(res["loss"])
-    plt.title("Loss")
-    plt.legend(["val_loss", "loss"])
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.show()
-    plt.figure(figsize=(12,4))
-    plt.plot(res["val_accuracy"])
-    plt.plot(res["accuracy"])
-    plt.title("Accuracy")
-    plt.legend(["val_accuracy", "accuracy"])
-    plt.xlabel("Epochs")
-    plt.ylabel("Accuracy")
-    plt.show()
 
 scaler = MinMaxScaler()
         
