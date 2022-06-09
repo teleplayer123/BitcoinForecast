543ca5a81d14a45eb194d327c77f02c63a076c14
cole sashkin <csashceh1024@gmail.com>
2022-06-09 08:49:54 -0700

    commit on main

--- btc_forecast.py
+++ btc_forecast.py
@@ -12,11 +12,11 @@ from utils import binary_classification, visualize_results, visualize_loss, trai
 
 FEATURE_COLUMNS = ["Time", "Low", "High", "Open", "Close", "Volume"]
 SEQ_LEN = 60
-FORECAST_STEP = 30
+FORECAST_STEP = 3
 BATCH_SIZE = 64
 EPOCHS = 10
    
-scaler = MinMaxScaler()
+#scaler = MinMaxScaler()
 
 df = pd.read_csv("data/BTC-USD.csv", names=FEATURE_COLUMNS)
 df.set_index("Time", inplace=True)
@@ -26,13 +26,13 @@ btc_df["Class"] = list(map(binary_classification, btc_df["Close"], btc_df["Forec
 btc_df.dropna(inplace=True)
 #btc_df["Close"] = scaler.fit_transform(btc_df[["Close"]])
 #btc_df["Volume"] = scaler.fit_transform(btc_df[["Volume"]])
-btc_df.dropna(inplace=True)
+#btc_df.dropna(inplace=True)
 
 X_train, y_train, X_test, y_test = train_test_split(btc_df, SEQ_LEN, 0.05, target_col="Class", remove_col="Forecast")
-print("X_train shape:  ", X_train.shape)
-print("y_train shape: ", y_train.shape)
-print("X_test shape: ", X_test.shape)
-print("y_test shape: ", y_test.shape)
+print("X_train shape:  ", np.array(X_train).shape)
+print("y_train shape: ", np.array(y_train).shape)
+print("X_test shape: ", np.array(X_test).shape)
+print("y_test shape: ", np.array(y_test).shape)
 
 SHAPE = f"{X_train.shape[1]}_{X_train.shape[2]}"
 UNITS = 128
@@ -78,12 +78,15 @@ checkpoint_path = os.path.join(cp_dir, cp_fn)
 cp_callback = ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1, save_freq=BATCH_SIZE*BATCH_SIZE)
 tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
 
-model = build_n_layer_model(UNITS, LAYERS, input_shape=(X_train.shape[1:]), n_out=3)
+model = cuda_lstm_model(np.array(X_train).shape[1:])
+#model = build_n_layer_model(UNITS, LAYERS, input_shape=(X_train.shape[1:]), n_out=3)
 #model.build((None, *X_train.shape[1:]))
 model.summary()
 model.save_weights(checkpoint_path.format(epoch=0))
-res = model.fit(X_train, y_train, epochs=EPOCHS,
-                validation_split=0.2, callbacks=[tensorboard, cp_callback])
-print(res.history)
+res = model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
+                validation_data=(X_test, y_test), callbacks=[tensorboard, cp_callback])
+scores = model.evaluate(X_test, y_test, verbose=0)
+print(f"Loss: {scores[0]}")
+print(f"Accuracy: {scores[1]}")
 visualize_results(res)
 model.save(f"models/{NAME}.h5")
